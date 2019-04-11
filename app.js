0a32a39804ba35cce3f39fcc5a0c5d87e2f5f327
yassinphilip <xaccrocheur@gmail.com>
2019-04-11 13:36:39 +0100

    Service Worker: Listing regs WIP

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js', { scope: '/' }).then((reg) => {
+
+        if(reg.installing) {
+            console.log('Service worker installing');
+        } else if(reg.waiting) {
+            console.log('Service worker installed');
+        } else if(reg.active) {
+            console.log('Service worker active');
+        }
+
+    }).catch(function(error) {
+        // registration failed
+        console.log('Registration failed with ' + error);
+    });
+}
+
 // if ('serviceWorker' in navigator) {
-//     navigator.serviceWorker.register('sw.js', { scope: '/' }).then(function(reg) {
-
-//         if(reg.installing) {
-//             console.log('Service worker installing');
-//         } else if(reg.waiting) {
-//             console.log('Service worker installed');
-//         } else if(reg.active) {
-//             console.log('Service worker active');
+//     navigator.serviceWorker.getRegistrations().then((registrations) => {
+//         for (let registration of registrations) {
+//             console.log('reg scriptURL: [%s] scope: %s', registration.active.scriptURL, registration.scope);
+//             // registration.unregister();
 //         }
-
-//     }).catch(function(error) {
-//         // registration failed
-//         console.log('Registration failed with ' + error);
 //     });
 // }
-
-if ('serviceWorker' in navigator) {
-
-    navigator.serviceWorker.getRegistrations().then(function(registrations) {
-        for(let registration of registrations) {
-            console.log('reg scriptURL: [%s] scope: %s', registration.active.scriptURL, registration.scope);
-            // registration.unregister();
-        }
-    });
-
-}
