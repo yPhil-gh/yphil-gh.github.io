86ad03a40edc8644e9d15474995dfb79df582b1f
yassinphilip <xaccrocheur@gmail.com>
2019-04-11 13:47:30 +0100

    Service Worker: Putting assets in cache OK

--- sw.js
+++ sw.js
@@ -1,3 +1,21 @@
-console.log(
-    "SW: I am alive"
-);
+const cacheName = 'v1';
+
+console.log('SW: I am alive, the cache is [%s]', cacheName);
+
+const cacheAssets = [
+    'index.html',
+    'style.css',
+    'favicon.ico'
+];
+
+
+self.addEventListener('install', (e) => {
+    console.log('Service worker: Installed');
+    e.waitUntil(
+        caches
+            .open(cacheName)
+            .then(cache => {
+                return cache.addAll(cacheAssets);
+            })
+    );
+});
