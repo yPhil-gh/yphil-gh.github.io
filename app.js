0266231b44a92680161a52d0f52bdf197b50efb8
yassinphilip <xaccrocheur@gmail.com>
2019-04-11 12:27:59 +0100

    Service Worker: uReg WIP

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
+// if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register('sw.js', { scope: '/' }).then(function(reg) {
+
+//         if(reg.installing) {
+//             console.log('Service worker installing');
+//         } else if(reg.waiting) {
+//             console.log('Service worker installed');
+//         } else if(reg.active) {
+//             console.log('Service worker active');
+//         }
+
+//     }).catch(function(error) {
+//         // registration failed
+//         console.log('Registration failed with ' + error);
+//     });
+// }
+
+
+
+// Also if navigator.serviceWorker
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js', { scope: '/' }).then(function(reg) {
+    console.log('ok');
 
-        if(reg.installing) {
-            console.log('Service worker installing');
-        } else if(reg.waiting) {
-            console.log('Service worker installed');
-        } else if(reg.active) {
-            console.log('Service worker active');
-        }
+    navigator.serviceWorker
+        .getRegistration()
+        .then(registration => {
+            if (registration) {
+                registration.unregister().then((boolean) => {
+                    console.log('UnReg :', boolean)
+                });
+            }
+        })
+        .catch(error => {
+            // Il y a eu un problème
+            console.error(
+                "App: Crash de Service Worker",
+                error
+            );
+        });
 
-    }).catch(function(error) {
-        // registration failed
-        console.log('Registration failed with ' + error);
-    });
 }
