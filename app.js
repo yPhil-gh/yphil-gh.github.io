31f1a20ebc41db8a49b9000b21d25d104139f646
yassinphilip <xaccrocheur@gmail.com>
2019-04-11 12:37:40 +0100

    Service Worker: Listing regs WIP

--- app.js
+++ app.js
@@ -1,36 +1,27 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js', { scope: '/' })
-        .then(function(reg) {
-
-            if(reg.installing) {
-                console.log('Service worker installing');
-            } else if(reg.waiting) {
-                console.log('Service worker installed');
-            } else if(reg.active) {
-                console.log('Service worker active');
-            }
-
-    }).catch(function(error) {
-        // registration failed
-        console.log('Registration failed with ' + error);
-    });
-}
-
 // if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register('sw.js', { scope: '/' }).then(function(reg) {
 
-//     navigator.serviceWorker
-//         .getRegistration()
-//         .then(registration => {
-//             if (registration) {
-//                 registration.unregister().then((boolean) => {
-//                     console.log('UnReg :', boolean);
-//                 });
-//             } else {
-//                 console.log('No SW registered');
-//             }
-//         })
-//         .catch(error => {
-//             console.log("App: Crash de Service Worker", error);
-//         });
+//         if(reg.installing) {
+//             console.log('Service worker installing');
+//         } else if(reg.waiting) {
+//             console.log('Service worker installed');
+//         } else if(reg.active) {
+//             console.log('Service worker active');
+//         }
 
+//     }).catch(function(error) {
+//         // registration failed
+//         console.log('Registration failed with ' + error);
+//     });
 // }
+
+if ('serviceWorker' in navigator) {
+
+    navigator.serviceWorker.getRegistrations().then(function(registrations) {
+        for(let registration of registrations) {
+            console.log('registration: [%s]', registration);
+            // registration.unregister();
+        }
+    });
+
+}
