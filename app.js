56fa1398fdb13a88c3ed1ee4e40e39bada21f3c2
yassinphilip <xaccrocheur@gmail.com>
2019-04-16 13:41:50 +0100

    Service Worker: cache-and-update: OK, no dynamic caching

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
+// Cache details
+var cacheDetails = document.getElementById('cacheDetails');
+cacheDetails.innerHTML = "plip";
+
+var sW = 'sw-cache-and-update.js';
+
+// function reload() {
+//     window.location.reload(true);
+// }
+
+// if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.getRegistrations().then((registrations) => {
+//         for (let registration of registrations) {
+//             console.log('REG scriptURL: [%s] scope: %s', registration.active.scriptURL, registration.scope);
+//             // if (registration.active.scriptURL == 'sw.js') {
+//             //     console.log('UNREG scriptURL: [%s] scope: %s', registration.active.scriptURL, registration.scope);
+//             //     registration.unregister();
+//             // }
+//         }
+//     });
+// }
+
 // Register the ServiceWorker limiting its action to those URLs starting by controlled. The scope is not a path but a prefix. First, it is converted into an absolute URL, then used to determine if a page is controlled by testing it is a prefix of the request URL.
 
-navigator.serviceWorker.register('sw-cache-update-and-refresh.js', {
+navigator.serviceWorker.register(sW, {
     scope: '/'
 });
 
-// Load controlled pages once the worker is active.
-
-// navigator.serviceWorker.ready.then(reload);
-
-// Reload on demand.
-var reloadButton = document.querySelector('#reload');
-reloadButton.onclick = reload;
-
-function reload() {
-    window.location.reload(true);
+function send_message_to_sw(msg){
+    navigator.serviceWorker.controller.postMessage("Client 1 says '"+msg+"'");
 }
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.getRegistrations().then((registrations) => {
-        for (let registration of registrations) {
-            if (registration.active.scriptURL == 'sw.js') {
-                console.log('UNREG scriptURL: [%s] scope: %s', registration.active.scriptURL, registration.scope);
-                registration.unregister();
-            }
-        }
+if('serviceWorker' in navigator){
+    // Handler for messages coming from the service worker
+    navigator.serviceWorker.addEventListener('message', function(event){
+        console.log("Client 1 Received Message: " + event.data);
+        event.ports[0].postMessage("Client 1 Says 'Hello back!'");
     });
 }
---
+++ sw-cache-and-update.js
@@ -0,0 +1,107 @@
+// https://serviceworke.rs/strategy-cache-and-update_service-worker_doc.html
+
+// Détails des fichiers à pre-cacher
+var cacheName = 'v2';
+
+console.log('SW: I am alive, the cache is [%s]', cacheName);
+
+var cacheAssets = [
+    'index.html',
+    'style.css',
+    'favicon.ico'
+];
+
+// Pre-cache à l'installation du SW
+
+self.addEventListener('install', function(evt) {
+    console.log('The service worker is being installed.');
+
+    // Le SW continue à installer jusqu'au return de la promise
+
+    evt.waitUntil(precache());
+
+});
+
+// On renvoie une promise résolue quand tous les fichiers ont été pre-cachés
+function precache() {
+    return caches.open(cacheName).then(function (cache) {
+        // send_message_to_all_clients('Hey yo!');
+        return cache.addAll(cacheAssets);
+    });
+}
+
+self.addEventListener('activate', (e) => {
+    console.log('Service worker: Activated');
+    // Remove unwanted caches
+    e.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if(cache !== cacheName){
+                        console.log('Service Worker: Clearing old cache: %s', cache);
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+addEventListener('fetch', event => {
+
+    // On intercepte la requete, et on la gere nous-mêmes.
+
+    event.respondWith(async function() {
+
+        // On regarde si elle est dans le cache
+
+        const cachedResponse = await caches.match(event.request);
+
+        // Si oui, on la returne
+        if (cachedResponse) return cachedResponse;
+        // Sinon, on sert la requete à partir du réseau
+        return fetch(event.request);
+    }());
+});
+
+// Envoi d'un MSG au client
+
+function msgClient(text) {
+    return self.clients.matchAll().then(function (clients) {
+        clients.forEach(function (client) {
+            var message = {
+                type: 'refresh',
+                text: text
+            };
+            client.postMessage(JSON.stringify(message));
+        });
+    });
+}
+
+function send_message_to_client(client, msg){
+    return new Promise(function(resolve, reject){
+        var msg_chan = new MessageChannel();
+
+        msg_chan.port1.onmessage = function(event){
+            if(event.data.error){
+                reject(event.data.error);
+            }else{
+                resolve(event.data);
+            }
+        };
+
+        client.postMessage("SW Says: '"+msg+"'", [msg_chan.port2]);
+    });
+}
+
+function send_message_to_all_clients(msg){
+    clients.matchAll().then(clients => {
+        clients.forEach(client => {
+            send_message_to_client(client, msg).then(m => console.log("SW Received Message: "+m));
+        });
+    });
+}
+
+self.addEventListener('message', function(event){
+    console.log("SW Received Message: " + event.data);
+});
